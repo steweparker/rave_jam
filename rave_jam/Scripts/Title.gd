28309ac272d19f7e8448d13f2dd0b9b36eeac6cf
Stewe <veryhungrylemon@gmail.com>
2022-01-07 15:00:49 +0200

    english localization added

--- rave_jam/Scripts/Title.gd
+++ rave_jam/Scripts/Title.gd
@@ -6,6 +6,22 @@ var main_scene = preload("res://Scenes/world.tscn")
 func _ready():
 	pass # Replace with function body.
 
-func _input(event):
+#func _input(event):
+	
+
+
+func _on_set_locale_toggled(button_pressed):
+	if button_pressed:
+		set_lang("ru")
+	else:
+		set_lang("en")
+
+
+func set_lang(lang):
+	TranslationServer.set_locale(lang)
+	$CanvasLayer/set_locale.text = lang
+
+
+func _on_TextureRect_gui_input(event):
 	if event is InputEventMouseButton and event.pressed:
 		get_tree().change_scene("res://Scenes/world.tscn")
